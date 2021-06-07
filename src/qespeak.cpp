3b2b4ef67c39eb5e655cd2002cd3e1ea107f2cc2
Leezhuocong <1123584734@qq.com>
2021-06-07 13:23:49 +0800

    Modify the qespeak.cpp file.

--- src/qespeak.cpp
+++ src/qespeak.cpp
@@ -1,6 +1,71 @@
-#include "qespeak.h"
+﻿#include "qespeak.h"
 
 QEspeak::QEspeak()
+{
+
+}
+
+QEspeak::~QEspeak()
+{
+
+}
+
+int QEspeak::setSpeakFile(QString filename)
+{
+
+}
+
+int QEspeak::setVoice(QString voice)
+{
+
+}
+
+int QEspeak::setRate(int rate)
+{
+
+}
+
+int QEspeak::setFormat(int format)
+{
+
+}
+
+int QEspeak::setSpeakText(QString text)
+{
+
+}
+
+void QEspeak::updateValume(int val)
+{
+
+}
+
+int QEspeak::saveToFile(QString filename)
+{
+
+}
+
+int QEspeak::speakText(QString text)
+{
+
+}
+
+int QEspeak::speakFile(QString filename)
+{
+
+}
+
+int QEspeak::togglePause()
+{
+
+}
+
+int QEspeak::stop()
+{
+
+}
+
+int QEspeak::reset()
 {
 
 }
