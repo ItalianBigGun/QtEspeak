36591f5b57645ba0d5855de4eca870c83a3784ea
意大利炮 <1123584734@qq.com>
2021-06-07 13:19:55 +0800

    Modify the qespeak.h file.

--- include/qespeak.h
+++ include/qespeak.h
@@ -17,11 +17,22 @@ class QEspeak
 public:
     QEspeak();
     virtual ~QEspeak();
+    int setSpeakFile(QString filename);
+    int setVoice(QString voice);
+    int setRate(int rate);
+    int setFormat(int format);
+    int setSpeakText(QString text);
+
 public slots:
     void updateValume(int val);
     int saveToFile(QString filename);
     int speakText(QString text);
     int speakFile(QString filename);
+    int togglePause();
+    int stop();
+    int reset();
+signals:
+    void sendEvent(int event_type);
 private:
     QString save_file_name;
     QString to_speak_text;
