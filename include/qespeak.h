cb495017f783cb96df0533fead93c6e88af4faeb
意大利炮 <1123584734@qq.com>
2021-06-07 11:36:30 +0800

    modify the QEspeak.h

--- include/qespeak.h
+++ include/qespeak.h
@@ -2,11 +2,31 @@
 #define QESPEAK_H
 
 #include <QObject>
+extern "C" {
+#include <speak_lib.h>
 
+#ifdef __WIN32__
+#include <windows.h>
+#define sleep(x) Sleep(1000*x)
+#endif
+}
 class QEspeak
 {
+    Q_OBJECT
+
 public:
     QEspeak();
+    virtual ~QEspeak();
+public slots:
+    void updateValume(int val);
+    int saveToFile();
+    int speakText(QString text);
+    int speakFile(QString filename);
+private:
+    QString save_file_name;
+    QString to_speak_text;
+    int valume;
+
 };
 
 #endif // QESPEAK_H
